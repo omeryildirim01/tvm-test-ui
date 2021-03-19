1f8e99b4b0aa2309a39601d5b9fa03a3c3d1603c
Ömer YILDIRIM <yildirimomer01@gmail.com>
2021-03-19 16:02:49 +0300

    commit
    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav';
+import Menu from './components/Menu';
+import Users from './admin/Users';
+import { BrowserRouter, Route } from "react-router-dom"
+import Main from './main/Main';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav />
+      <div className="container-fluid">
+        <div className="row">
+          <Menu />
+          <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+            <BrowserRouter>
+            <Route path='/' component={Main}/>
+              <Route path='/admin/users' component={Users} />
+            </BrowserRouter>
+          </main>
+        </div>
+
+      </div>
     </div>
   );
 }
